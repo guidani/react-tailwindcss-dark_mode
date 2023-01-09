a8cecf078b1fc6a22d35f3863da39b250b17d8fd
guilherme daniel <gui.dani13@gmail.com>
2023-01-08 22:15:09 -0300

    update visual

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,9 @@ function App() {
   const [darkMode, setDarkMode] = useState(false);
   return (
     <div className={darkMode ? "dark" : ""}>
-      <div className="w-full min-h-screen bg-white flex justify-center items-center flex-col dark:bg-gray-900">
-        <h1 className="text-6xl font-bold dark:text-white">Dark Mode</h1>
+      <nav className="w-full flex justify-end bg-transparent p-2 fixed">
         <button
-          className="bg-black dark:bg-white text-white dark:text-black text-4xl px-5 py-2 font-bold rounded-md mt-4"
+          className="bg-black dark:bg-white text-white dark:text-black px-5 py-2 font-bold rounded-md"
           onClick={() => setDarkMode(!darkMode)}
         >
           {darkMode ? (
@@ -17,6 +16,9 @@ function App() {
             <BsFillMoonStarsFill size={30} color="white" />
           )}
         </button>
+      </nav>
+      <div className="w-full min-h-screen bg-white flex justify-center items-center flex-col dark:bg-gray-900">
+        <h1 className="text-6xl font-bold dark:text-white">Dark Mode</h1>
       </div>
     </div>
   );
